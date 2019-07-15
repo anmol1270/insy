c00646ea238a0bc3bb589de74c1747026c162078
anmol1270@gmail.com <anmol1270@gmail.com>
2019-07-15 19:17:26 +0530

    added counter

---
+++ AndroidStudioProjects/Insycon2/app/src/main/java/com/example/anmolnarang/insycon/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.anmolnarang.insycon;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int CurrentPage=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+
+
+
+}
